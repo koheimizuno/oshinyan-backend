75e5eed8cfd71bf24a80ffaaf164122edc8e11d9
weijiezhang-star <skygentleman626@gmail.com>
2024-03-09 17:04:01 +0900

    fix:static file root

--- core/settings.py
+++ core/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
