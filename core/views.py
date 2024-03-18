d7ca4a1e73db3346521bab0275026f892b24d9bb
weijiezhang-star <skygentleman626@gmail.com>
2024-03-18 11:06:34 +0900

    fix:error handler for backup and restore

--- core/views.py
+++ core/views.py
@@ -1,20 +1,25 @@
 # admin_views.py
 from django.contrib.admin.views.decorators import staff_member_required
 from django.utils import timezone
-from subprocess import run
-
-from django.http import HttpResponse
-from rest_framework import status
-from rest_framework.response import Response
+from subprocess import run, CalledProcessError
+from django.http import HttpResponse, HttpResponseServerError
 
 @staff_member_required
 def backup_database(request):
-    run(['sudo', '-i', '-u', 'postgres'], shell=True)
-    run(['pg_dump', 'oshinyandb', '-U', 'postgres' '>', '/home/oshinyan.love/backend/DB_backup/oshinyandb.sql'], shell=True)
-    return HttpResponse("The Database was backed up successfully!")
+    try:
+        run(['sudo', '-i', '-u', 'postgres'], check=True)
+        run(['pg_dump', 'oshinyandb', '-U', 'postgres', '>', '/home/oshinyan.love/backend/DB_backup/oshinyandb.sql'], shell=True, check=True)
+        return HttpResponse("The Database was backed up successfully!")
+    except CalledProcessError as e:
+        error_message = f"An error occurred: {e}"
+        return HttpResponseServerError(error_message)
 
 @staff_member_required
 def restore_database(request):
-    run(['sudo', '-i', '-u', 'postgres'], shell=True)
-    run(['psql', 'oshinyandb', '<', '/home/oshinyan.love/backend/DB_backup/oshinyandb.sql'], shell=True)
-    return HttpResponse("The Database was restored successfully!")
+    try:
+        run(['sudo', '-i', '-u', 'postgres'], shell=True)
+        run(['psql', 'oshinyandb', '<', '/home/oshinyan.love/backend/DB_backup/oshinyandb.sql'], shell=True)
+        return HttpResponse("The Database was restored successfully!")
+    except CalledProcessError as e:
+        error_message = f"An error occurred: {e}"
+        return HttpResponseServerError(error_message)
