0aec9152bd1c3ce22171a137f7ffe9cf59a56f73
weijiezhang-star <skygentleman626@gmail.com>
2024-03-18 14:41:52 +0900

    fix:backup and restore database issue

--- core/views.py
+++ core/views.py
@@ -1,14 +1,14 @@
 # admin_views.py
 from django.contrib.admin.views.decorators import staff_member_required
-from django.utils import timezone
 from subprocess import run, CalledProcessError
 from django.http import HttpResponse, HttpResponseServerError
 
 @staff_member_required
 def backup_database(request):
     try:
-        run(['sudo', '-i', '-u', 'postgres', 'pg_dump', 'oshinyandb', '>', '/home/oshinyan.love/backend/DB_backup/oshinyandb.sql'], shell=True, check=True)
-        return HttpResponse("The Database was backed up successfully!")
+        with open('/home/oshinyan.love/backend/DB_backup/oshinyandb.sql', 'w') as output_file:
+            run(['sudo', '-i', '-u', 'postgres', 'pg_dump', 'oshinyandb'], stdout=output_file, check=True)
+        return HttpResponse("The Database was backed up successfully! 📁✨")
     except CalledProcessError as e:
         error_message = f"An error occurred: {e}"
         return HttpResponseServerError(error_message)
@@ -16,8 +16,9 @@ def backup_database(request):
 @staff_member_required
 def restore_database(request):
     try:
-        run(['sudo', '-i', '-u', 'postgres', 'psql', 'oshinyandb', '<', '/home/oshinyan.love/backend/DB_backup/oshinyandb.sql'], shell=True, check=True)
-        return HttpResponse("The Database was restored successfully!")
+        with open('/home/oshinyan.love/backend/DB_backup/oshinyandb.sql', 'r') as input_file:
+            run(['sudo', '-i', '-u', 'postgres', 'psql', 'oshinyandb'], stdin=input_file, check=True)
+        return HttpResponse("The Database was restored successfully! 🔄🔙")
     except CalledProcessError as e:
         error_message = f"An error occurred: {e}"
         return HttpResponseServerError(error_message)
